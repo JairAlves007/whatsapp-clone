2b0674e83e0d422ba90c62c95d86e9f44f5af171
JairAlves007 <ajair2550@gmail.com>
2022-11-05 18:05:49 -0300

    ajustando imagem

--- src/components/ChatIntro/index.jsx
+++ src/components/ChatIntro/index.jsx
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function ChatIntro() {
 	return (
     <div className="chatIntro">
-      <img src="/assets/chatIntroIcon.jpg" alt="Chat Intro Image" />
+      <img src="/assets/chatIntroIcon.png" alt="Chat Intro Image" />
       <h1>Mantenha seu celular conectado</h1>
       <h2>
         O WhatsApp conecta ao seu telefone para sincronizar suas mensagens. 
